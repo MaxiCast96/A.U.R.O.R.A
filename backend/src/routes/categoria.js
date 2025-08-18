ca1d11272fc4577ec33eb0ca4ab93b070ebfbc1b
OpticaLaInteligente <opticalainteligente@gmail.com>
2025-08-18 17:57:02 -0600

    cambios de optimizacion, cambios de diseño y implementacion de rutas dependiendo de rol, tambien implemento lo que es algo de Auditoria para Admin y Gerente

--- backend/src/routes/categoria.js
+++ backend/src/routes/categoria.js
@@ -1,19 +1,19 @@
 // ===== RUTAS CATEGORIA =====
 import express from "express";
 import CategoriaController from "../controllers/CategoriaController.js";
+import { authenticateToken } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 // Rutas principales CRUD para categorías
 router.route("/")
     .get(CategoriaController.getCategoria) // GET /api/categoria - Obtener todas las categorías
-    .post(CategoriaController.createCategoria); // POST - Crear con icono
+    .post(authenticateToken, CategoriaController.createCategoria); // POST - Crear con icono
 
 // Rutas para manejo de categoría específica por ID
 router.route("/:id")
     .get(CategoriaController.getCategoriaById) // GET /api/categoria/:id - Obtener por ID
-    .put(CategoriaController.updateCategoria) // PUT - Actualizar con icono
-    .delete(CategoriaController.deleteCategoria); // DELETE /api/categoria/:id - Eliminar
+    .put(authenticateToken, CategoriaController.updateCategoria) // PUT - Actualizar con icono
+    .delete(authenticateToken, CategoriaController.deleteCategoria); // DELETE /api/categoria/:id - Eliminar
 
 export default router;
-
