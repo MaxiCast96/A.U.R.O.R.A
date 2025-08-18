8a3d23107ac5ed92de6d726f6b9fa814780f32e9
Maxi <chanakane820@gmail.com>
2025-08-18 16:04:50 -0600

    configuración de vercel 4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-  server: {
-    proxy: {
-      '/api': 'https://a-u-r-o-r-a.onrender.com',
-    },
-  },
+  plugins: [wasm(), topLevelAwait()],
+  css: {
+    transformer: 'lightningcss-wasm'
+  }
 })
